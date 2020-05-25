304f7cb8229c1e409583631adf75a54a9a165587
Dmitry <sax-ru@mail.ru>
2020-05-26 00:46:48 +0300

    day 1 + dz + dz*

---
+++ script/main.js
@@ -0,0 +1,54 @@
+const leftMenu = document.querySelector('.left-menu');
+const hamburger = document.querySelector('.hamburger');
+
+
+// открытие/закрытие меню
+
+hamburger.addEventListener('click', () => {
+    leftMenu.classList.toggle('openMenu');
+    hamburger.classList.toggle('open');
+});
+
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (!target.closest('.left-menu')) {  //closest - поднимается вверх по DOM дереву от того места куда кликнули и ищет указанный селектор и возвращает найденный элемент, если не находит то вернет null
+        console.log('клик не в меню');
+        leftMenu.classList.remove('openMenu');
+        hamburger.classList.remove('open');
+    }
+});
+
+// drop-menu используя делегирование
+leftMenu.addEventListener('click', event => {
+    const target = event.target;
+    const dropdown = target.closest('.dropdown');
+    if (dropdown) {
+        dropdown.classList.toggle('active');
+        leftMenu.classList.add('openMenu');
+        hamburger.classList.add('open');
+    }
+});
+
+
+const tvcardImg = document.querySelectorAll('.tv-card__img');
+
+tvcardImg.forEach(function (elem) {
+    elem.addEventListener('mouseover', (event) => {
+        console.log('mouseenter');
+        const target = event.target;
+        const src = target.getAttribute('src');
+
+        console.log(src);
+        target.dataset.srcOrigin = `${src}`;
+
+        const data = target.dataset.backdrop;
+        target.setAttribute('src', `${data}`);
+    });
+
+    elem.addEventListener('mouseout', (event) => {
+        console.log('mouseout');
+        const src = event.target.dataset.srcOrigin;
+        event.target.setAttribute('src', `${src}`);
+    });
+});
+
